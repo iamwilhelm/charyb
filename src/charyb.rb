5b2db417d2af190e4d7f07b364e30c6379e3d7b2
Wilhelm Chung <wil@frogmetrics.com>
2008-11-27 00:46:50 -0500

    remove exceptions from friend interations

--- src/charyb.rb
+++ src/charyb.rb
@@ -52,18 +52,15 @@ module Charyb
         next if friend.status.nil? || friend.status.text.empty?
         yield Friend::Twitter.new(friend)
       end
-    rescue Exception => e
-      puts e.inspect
     end
 
     def facebook_friends(session)
+      debugger
       friends = session.user.friends!(:name, :status) << session.user
       friends.each do |friend|
         next if friend.status.message.nil? || friend.status.message.empty?
         yield Friend::Facebook.new(friend)
       end
-    rescue Exception => e
-      puts e.inspect
     end
 
 
