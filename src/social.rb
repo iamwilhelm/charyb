b9bf319e92c1f0b0beb4eec83bfa3d6d7a3513c1
Wilhelm Chung <wil@frogmetrics.com>
2008-11-29 22:20:48 -0500

    updated facebook login

--- src/social.rb
+++ src/social.rb
@@ -4,39 +4,53 @@ require 'rubygems'
 require 'facebooker'
 require 'twitter'
 
+ENV['FACEBOOK_API_KEY'] = "47d25ddec840c0df9f27abba9ee9a2cf"
+ENV['FACEBOOK_SECRET_KEY'] = "57adfe16a3ce0358fa56fbc94d90e982"
+
 module Social
 
   module Facebook
-    API_KEY = "47d25ddec840c0df9f27abba9ee9a2cf"
-    SECRET_KEY = "57adfe16a3ce0358fa56fbc94d90e982"
     SESSION_PATH = File.join(File.expand_path(File.dirname(__FILE__)), "../tmp")
 
     class << self
       def login
+        # load any previous session otherwise create a new one
+        @session = load_session
+
         # open the session
         begin
-          puts "Loading saved session from file"
-          File.open(File.join(SESSION_PATH, 'session.fb'), 'r') do |file|
-            @session = Marshal.load(file)
-          end
           @session.user.name
         rescue 
           puts "Facebook session loading failed."
-          puts "Creating new session"
-          @session = Facebooker::Session::Desktop.create(API_KEY, SECRET_KEY)
+          puts "Creating new session..."
+          @session = Facebooker::Session::Desktop.create
           puts "Paste the URL into your web browser and login:"
           puts @session.login_url
           puts "Hit return to continue..."
           gets
-
-          # save the session
-          puts "Saving session file at #{SESSION_PATH}"
-          File.open(File.join(SESSION_PATH, 'session.fb'), 'w') do |file|
-            Marshal.dump(@session, file)
-          end
+          retry
         end
+        save_session
 
         yield @session
+      end
+
+      private
+      def load_session
+        begin
+          File.open(File.join(SESSION_PATH, 'session.fb'), 'r') do |file|
+            return Marshal.load(file)
+          end
+        rescue
+          return Facebooker::Session::Desktop.create
+        end
+      end
+
+      def save_session
+        puts "Saving session file at #{SESSION_PATH}"
+        File.open(File.join(SESSION_PATH, 'session.fb'), 'w') do |file|
+          Marshal.dump(@session, file)
+        end
       end
     end
   end
