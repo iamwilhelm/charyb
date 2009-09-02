8a9e46e3c177b4c1354e3b5179b3469871fb4a79
Wil Chung <iamwil@gmail.com>
2009-09-02 08:58:49 -0400

    create a simple web interface skeleton

--- src/web/web.rb
+++ src/web/web.rb
@@ -3,8 +3,13 @@ require 'sinatra'
 require 'active_record'
 
 require 'erb'
+require 'source'
 
 get '/' do
   erb :index
 end
 
+get '/sources' do
+  @sources = Charyb::Source.find(:all)
+  erb :"sources/index"
+end
