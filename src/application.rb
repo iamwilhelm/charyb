53e0a0983e4978d5b90054f9f030f74147379c41
Wil Chung <iamwil@gmail.com>
2009-09-06 16:28:35 -0400

    changed references to sources to be more explicit as "datasources"

--- src/application.rb
+++ src/application.rb
@@ -19,6 +19,11 @@ configure do
   mime :json, "application/json"
 end
 
+######### Global Filters ##########
+
+
+######### Front page routes ##########
+
 # new datasource.  The front page for entering urls
 get '/' do
   erb :"/index"
@@ -27,63 +32,65 @@ end
 ########## Datasource routes ##########
 
 # lists data sources in the db
-get '/sources' do
-  @sources = Charyb::SourceTracker.datasources
-  erb :"/sources/index"
+get '/datasources' do
+  @datasources = Charyb::SourceTracker.datasources
+  erb :"/datasources/index"
 end
 
 # shows a created data source
-get '/sources/:id' do
-  @source = Models::Datasource.find(params["id"], :include => ["cols"])
-  @ds_response = open(@source.url) { |f| f.read }
+get '/datasources/:id' do
+  @datasource = Models::Datasource.find(params["id"], :include => ["cols"])
+  @ds_response = open(@datasource.url) { |f| f.read }
 
-  case @source.content_type
+  case @datasource.content_type
   when "text/html"
     @doc = Hpricot(@ds_response)
   when "application/xml"
     @doc = Hpricot::XML(@ds_response)
   end
 
-  erb :"/sources/show"
+  erb :"/datasources/show"
 end
 
 # creates a data source
-post '/sources' do
+post '/datasources' do
   # TODO this can be refactored into the source tracker
-  @source = Models::Datasource.find_by_url(params["source"]["url"])
-  if @source.nil?
+  @datasource = Models::Datasource.find_by_url(params["source"]["url"])
+  if @datasource.nil?
     response = open(params["source"]["url"])
-    @source = Models::Datasource.create!("url" => params["source"]["url"], 
+    @datasource = Models::Datasource.create!("url" => params["source"]["url"], 
                                          "content_type" => response.content_type)
   end
-  redirect "/sources/#{@source.id}"
+  redirect "/datasources/#{@datasource.id}"
 end
 
 # editing data source ajax
-get '/sources/:id/edit' do
-  @source = Models::Datasource.find(params["id"])
-  erb :"/sources/edit", :layout => false
+get '/datasources/:id/edit' do
+  @datasource = Models::Datasource.find(params["id"])
+  erb :"/datasources/edit", :layout => false
 end
 
 # updating data source ajax
-put '/sources/:id' do
-  @source = Models::Datasource.find(params["id"])
-  @source.update_attributes!(params["source"])
-  erb :"sources/_source", :layout => false
+put '/datasources/:id' do
+  @datasource = Models::Datasource.find(params["id"])
+  @datasource.update_attributes!(params["source"])
+
+  redirect back
+  # erb :"/datasources/_source", :layout => false
 end
 
 ########## Column routes ##########
 
 # new column ajax
-get '/sources/:source_id/cols/new' do
-  @source = Models::Datasource.find(params["source_id"])
-  @col = @source.cols.new
+get '/datasources/:source_id/cols/new' do
+  @datasource = Models::Datasource.find(params["source_id"])
+  @col = @datasource.cols.new
   erb :"/cols/new", :layout => false 
 end
 
 # create column ajax
-post '/sources/:source_id/cols' do
-  @source = Models::Datasource.find(params["source_id"])
-  @col = @source.cols.new(params["col"])
+post '/datasources/:source_id/cols' do
+  @datasource = Models::Datasource.find(params["source_id"])
+  @col = @datasource.cols.new(params["col"])
   # ??
 end
