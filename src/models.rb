7637f14d6a4bf3a2d5168bf4e5eed20a5b77449c
Wil Chung <iamwil@gmail.com>
2009-09-06 15:08:04 -0400

    removes debugging printout of which models got loaded

--- src/models.rb
+++ src/models.rb
@@ -1,6 +1,5 @@
 # require this file makes it easy to require all models by default
 Dir.glob(File.join(File.dirname(__FILE__), "models/*.rb")) do |model_file|
-  puts model_file
   require model_file
 end
 
