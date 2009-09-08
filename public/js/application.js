c54525d6adc52e0e046a641571dc124d2a25505e
Wil Chung <iamwil@gmail.com>
2009-09-07 23:57:53 -0400

    implemented rest_get lib to replace link_to_remote-like function in rails

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
 
+    /* Helper function to return all the data- attributes from the element
+     * and returns a hash of all elements
+     */
+    parse_data_attrs = function(elem) {
+        var hash = new Object();
+        var attr_array = elem.attributes;
+        
+        // filter all attributes that start with "data-" and create
+        // 
+        $.each($.grep(attr_array, function(attr, i) {
+                          return attr.name.match(/^data-/);
+                      }), 
+               function() {
+                   hash[this.name.replace(/^data-/, "")] = this.value;
+               });
+
+        return hash;
+    }
+
     /* Makes an GET ajax link and posts the result from server in the target 
      * DOM element
      * 
      * It has a couple parameters it uses:
      * 
-     *   * data-href - The URL where to make the AJAX request
+     *   * data-action - The URL where to make the AJAX request
      *   * data-target - Which DOM element to insert the results of the request
      *        Use JQuery selectors to target the element
      *   * data-effect - The jquery effect to use to transition in the new 
@@ -19,12 +38,15 @@ $(document).ready(function() {
      *               data-target="#post_new"
      *   </a>
      */
-    $("a.get").click(function() {
-        url = $(this).attr("data-href");
-        target = $(this).attr("data-target");
-        $.get(url, function(data) { 
-            $(target).html(data);
+    $("a[rel*=rest_get]").click(
+        function() {
+            var data = parse_data_attrs(this);
+
+            $(data.target).html("Loading...");
+
+            $.get(data.action, function(response_html) { 
+                      $(data.target).html(response_html);
+                  });
         });
-    });
 
 });
